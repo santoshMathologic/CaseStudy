b3f2fe70907b4a108632a4f81617925aa382c986
rakesh <rak1994kumar@gmail.com>
2016-09-03 23:59:41 -0700

    project Stand

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,14 +1,67 @@
-angular.module("caseStudyApp", [
+'use strict';
+
+var initInjector = angular.injector(['ng']);
+var $http = initInjector.get('$http');
+
+angular
+  .module('caseStudyApp', [
     'oc.lazyLoad',
     'ui.router',
     'ui.bootstrap',
     'angular-loading-bar',
-]).config(['$routeProvider', '$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider', function ($routeProvider, $stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
+    'ngSanitize',
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch',
+    'toggle-switch'
+  ])
+  .config(['$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider', function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
+
+    $ocLazyLoadProvider.config({
+      debug: false,
+      events: true,
+    });
+
+
+    //
+    $urlRouterProvider.otherwise('/dashboard/home');
 
 
 
+    $stateProvider
+      .state('dashboard', {
+        url: '/dashboard',
+        templateUrl: 'views/dashboard/main.html',
+        resolve: {
+          loadMyDirectives: function ($ocLazyLoad) {
+            return $ocLazyLoad.load(
+              {
+                name: 'caseStudyApp',
+                files: [
+
+                ]
+              })
+           }
+        }
+      })
+
+      .state('dashboard.home', {
+        url: '/home',
+        templateUrl: 'views/dashboard/home.html',
+        resolve: {
+          loadMyFiles: function ($ocLazyLoad) {
+            return $ocLazyLoad.load({
+              name: 'caseStudyApp',
+              files: [
+    
+              ]
+            })
+          }
+        }
+      })
+
+  }]);
 
 
-
-
-}]);
