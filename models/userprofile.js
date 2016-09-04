4e684cb0c04e06a5a9eeb222acc7928db5bb650a
rakesh <rak1994kumar@gmail.com>
2016-09-04 12:06:11 -0700

    'mongoose-long' added

--- models/userprofile.js
+++ models/userprofile.js
@@ -1,15 +1,18 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var SchemaTypes = mongoose.Schema.Types;
+
+require('mongoose-long')(mongoose);
 mongoose.set('debug', true);
 var userSchema = new mongoose.Schema({
     userName: {type:String,index: true},
     firstName:String,
     lastName : String,
-    password: {type:String,index: true},
+    password: {type:String},
     email: String,
     city: String,
     address:String,
-    mobileNo:Number,
+    mobileNo : {type: SchemaTypes.Long, min: 0, default: 0}
   
     
 });
